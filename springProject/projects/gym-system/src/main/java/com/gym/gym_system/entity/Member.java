c306efb0cb29b61235755c1aefd1f0d9c4b2e910
mahmoud <1522003ma@gmail.com>
2025-04-29 05:40:31 +0300

    Initial commit

---
+++ springProject/projects/gym-system/src/main/java/com/gym/gym_system/entity/Member.java
@@ -0,0 +1,36 @@
+package com.gym.gym_system.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Table(name = "members")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String phone;
+
+    private String address;
+}
+
